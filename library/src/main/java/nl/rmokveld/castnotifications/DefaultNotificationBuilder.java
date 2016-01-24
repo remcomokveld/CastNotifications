894a5620cbf2f85709f82854620b976ac0a8c373
Remco Mokveld <remco@thecapitals.nl>
2016-01-25 00:02:02 +0100

    add search chromecasts button to wear notifications

--- library/src/main/java/nl/rmokveld/castnotifications/DefaultNotificationBuilder.java
+++ library/src/main/java/nl/rmokveld/castnotifications/DefaultNotificationBuilder.java
@@ -6,6 +6,8 @@ import android.support.v4.app.NotificationCompat;
 
 class DefaultNotificationBuilder implements NotificationBuilder {
 
+    private boolean mSearchActionOnWear = true;
+
     @Override
     public void build(Context context, NotificationCompat.Builder builder, int id, String title, String subtitle, boolean castDevicesAvailable) {
         builder.setContentTitle(title)
@@ -13,6 +15,12 @@ class DefaultNotificationBuilder implements NotificationBuilder {
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
+        if (mSearchActionOnWear && !castDevicesAvailable) {
+            // If there are no notifications add an action on wear to start active discovery. Because of the wakelocks the device will wake up which is needed for discovery
+            builder.extend(new NotificationCompat.WearableExtender()
+                    .addAction(new NotificationCompat.Action(R.drawable.ic_cast_light, context.getString(R.string.cast_notifications_discovering),
+                            PendingIntent.getService(context, 0, DiscoveryService.buildIntent(context, true, "wear"), PendingIntent.FLAG_UPDATE_CURRENT))));
+        }
         if (subtitle != null)
             builder.setContentText(subtitle);
     }
@@ -25,7 +33,9 @@ class DefaultNotificationBuilder implements NotificationBuilder {
 
     @Override
     public void buildForError(Context context, NotificationCompat.Builder builder, int id, String title, String subtitle) {
+        mSearchActionOnWear = false;
         build(context, builder, id, title, subtitle, false);
+        mSearchActionOnWear = true;
     }
 
 
