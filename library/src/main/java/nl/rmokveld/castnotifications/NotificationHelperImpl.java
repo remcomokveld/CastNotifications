0f190c6cbd31f5d75b76ce7969ae7ea509f14b4b
Remco Mokveld <remco@thecapitals.nl>
2016-01-25 23:28:21 +0100

    Add null check for castDevices in NotificationHelper

--- library/src/main/java/nl/rmokveld/castnotifications/NotificationHelperImpl.java
+++ library/src/main/java/nl/rmokveld/castnotifications/NotificationHelperImpl.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.NotificationCompat;
 import android.util.SparseArray;
@@ -23,13 +24,13 @@ class NotificationHelperImpl implements NotificationHelper {
     }
 
     @Override
-    public void add(CastNotification castNotification, Map<String, String> availableRoutes) {
+    public void add(CastNotification castNotification, @Nullable Map<String, String> availableRoutes) {
         mCastNotifications.put(castNotification.getId(), castNotification);
         postNotification(castNotification, availableRoutes);
     }
 
     @Override
-    public void addAll(List<CastNotification> castNotifications, Map<String, String> availableRoutes) {
+    public void addAll(List<CastNotification> castNotifications, @Nullable Map<String, String> availableRoutes) {
         for (CastNotification castNotification : castNotifications) {
             mCastNotifications.put(castNotification.getId(), castNotification);
         }
@@ -37,15 +38,17 @@ class NotificationHelperImpl implements NotificationHelper {
     }
 
     @Override
-    public void postNotification(CastNotification castNotification, Map<String, String> castDevices) {
+    public void postNotification(CastNotification castNotification, @Nullable Map<String, String> castDevices) {
         Log.d(TAG, "postNotification() called with: " + "castNotification = [" + castNotification + "], castDevices = [" + castDevices + "]");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
-        CastNotificationManager.getInstance().getNotificationBuilder().build(mContext, builder, castNotification.getId(), castNotification.getTitle(), castNotification.getContentText(), castNotification.getTimestamp(), castNotification.getCustomData(), castDevices.size() > 0);
+        CastNotificationManager.getInstance().getNotificationBuilder().build(mContext, builder, castNotification.getId(), castNotification.getTitle(), castNotification.getContentText(), castNotification.getTimestamp(), castNotification.getCustomData(), castDevices != null && castDevices.size() > 0);
         builder.setDeleteIntent(PendingIntent.getBroadcast(mContext, 0, new Intent(mContext, NotificationDeletedReceiver.class).setData(Uri.parse("content://" + BuildConfig.APPLICATION_ID + "/notifications/" + castNotification.getId())), PendingIntent.FLAG_UPDATE_CURRENT));
-        for (String routeId : castDevices.keySet()) {
-            builder.addAction(R.drawable.ic_cast_light, castDevices.get(routeId),
-                    PendingIntent.getService(mContext, castNotification.getId(), StartCastService.getIntent(castNotification, mContext, routeId, castDevices.get(routeId)),
-                            PendingIntent.FLAG_UPDATE_CURRENT));
+        if (castDevices != null) {
+            for (String routeId : castDevices.keySet()) {
+                builder.addAction(R.drawable.ic_cast_light, castDevices.get(routeId),
+                        PendingIntent.getService(mContext, castNotification.getId(), StartCastService.getIntent(castNotification, mContext, routeId, castDevices.get(routeId)),
+                                PendingIntent.FLAG_UPDATE_CURRENT));
+            }
         }
         NotificationManagerCompat.from(mContext).notify("cast_notifications", castNotification.getId(), builder.build());
     }
