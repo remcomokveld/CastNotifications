02fac908f7fa495a53ce980f708b15d991507832
Remco Mokveld <remco@thecapitals.nl>
2016-01-26 18:05:10 +0100

    Do not post a new notification if the routes are the same and the notifications have not been changed

--- library/src/main/java/nl/rmokveld/castnotifications/NotificationHelperImpl.java
+++ library/src/main/java/nl/rmokveld/castnotifications/NotificationHelperImpl.java
@@ -18,6 +18,8 @@ class NotificationHelperImpl implements NotificationHelper {
 
     private final Context mContext;
     private final SparseArray<CastNotification> mCastNotifications = new SparseArray<>();
+    private final NotificationChangeHelper mNotificationChangeHelper = new NotificationChangeHelper();
+
 
     public NotificationHelperImpl(Context context) {
         mContext = context.getApplicationContext();
@@ -25,19 +27,26 @@ class NotificationHelperImpl implements NotificationHelper {
 
     @Override
     public void add(CastNotification castNotification, @Nullable Map<String, String> availableRoutes) {
+        mNotificationChangeHelper.setDevices(availableRoutes);
         mCastNotifications.put(castNotification.getId(), castNotification);
+        mNotificationChangeHelper.notifyNotificationsChanged();
         postNotification(castNotification, availableRoutes);
+        mNotificationChangeHelper.reset();
     }
 
     @Override
     public void addAll(List<CastNotification> castNotifications, @Nullable Map<String, String> availableRoutes) {
+        mNotificationChangeHelper.setDevices(availableRoutes);
         for (CastNotification castNotification : castNotifications) {
             mCastNotifications.put(castNotification.getId(), castNotification);
         }
+        mNotificationChangeHelper.notifyNotificationsChanged();
         postNotifications(availableRoutes);
+        mNotificationChangeHelper.reset();
     }
 
     private void postNotification(CastNotification castNotification, @Nullable Map<String, String> castDevices) {
+        if (!mNotificationChangeHelper.shouldPostNotifications()) return;
         Log.d(TAG, "postNotification() called with: " + "castNotification = [" + castNotification + "], castDevices = [" + castDevices + "]");
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
         CastNotificationManager.getInstance().getNotificationBuilder().build(mContext, builder, castNotification.getId(), castNotification.getTitle(), castNotification.getContentText(), castNotification.getTimestamp(), castNotification.getCustomData(), castDevices != null && castDevices.size() > 0);
@@ -54,9 +63,11 @@ class NotificationHelperImpl implements NotificationHelper {
 
     @Override
     public void postNotifications(Map<String, String> availableRoutes) {
+        mNotificationChangeHelper.setDevices(availableRoutes);
         for (int i = 0; i < mCastNotifications.size(); i++) {
             postNotification(mCastNotifications.valueAt(i), availableRoutes);
         }
+        mNotificationChangeHelper.reset();
     }
 
     @Override
@@ -68,5 +79,40 @@ class NotificationHelperImpl implements NotificationHelper {
     public void cancel(int notificationId) {
         NotificationManagerCompat.from(mContext).cancel("cast_notifications", notificationId);
         mCastNotifications.delete(notificationId);
+    }
+
+    private static class NotificationChangeHelper {
+        private boolean mNotificationDirty = false;
+        private Map<String, String> mLastKnownDevices;
+
+        void notifyNotificationsChanged() {
+            mNotificationDirty = true;
+        }
+
+        public void reset() {
+            mNotificationDirty = false;
+        }
+
+        void setDevices(Map<String, String> devices) {
+            try {
+                if (mNotificationDirty || (devices == null && mLastKnownDevices == null)) return;
+                if (mLastKnownDevices == null || devices == null || mLastKnownDevices.size() != devices.size()) {
+                    mNotificationDirty = true;
+                } else {
+                    for (String s : mLastKnownDevices.keySet()) {
+                        if (!devices.keySet().contains(s)) {
+                            mNotificationDirty = true;
+                            return;
+                        }
+                    }
+                }
+            } finally {
+                mLastKnownDevices = devices;
+            }
+        }
+
+        boolean shouldPostNotifications() {
+            return mNotificationDirty;
+        }
     }
 }
