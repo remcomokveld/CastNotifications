46fb5891040e35d71701df4789c38e5662223d84
Remco Mokveld <remco@thecapitals.nl>
2016-01-26 18:08:10 +0100

    only update wifi state if there actually was a change

--- library/src/main/java/nl/rmokveld/castnotifications/WifiStateReceiver.java
+++ library/src/main/java/nl/rmokveld/castnotifications/WifiStateReceiver.java
@@ -8,15 +8,20 @@ import android.net.wifi.WifiManager;
 public class WifiStateReceiver extends BroadcastReceiver {
 
     private static final String TAG = "WifiStateReceiver";
+    private Boolean mWasConnected;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
             Log.d(TAG, "onWifiStateChanged()");
-            if (DeviceStateHelper.isWifiConnected(context)) {
-                CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiConnected();
-            } else {
-                CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiDisconnected();
+            boolean isConnected = DeviceStateHelper.isWifiConnected(context);
+            if (mWasConnected == null || mWasConnected != isConnected) {
+                if (isConnected) {
+                    CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiConnected();
+                } else {
+                    CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiDisconnected();
+                }
+                mWasConnected = isConnected;
             }
         }
     }
