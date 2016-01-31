a4efc4e25a5c2fa66ae7bc77ad0a61d464cbb88a
Remco Mokveld <remco@thecapitals.nl>
2016-01-31 22:09:27 +0100

    Use a static field for tracking wifi connected state

--- library/src/main/java/nl/rmokveld/castnotifications/WifiStateReceiver.java
+++ library/src/main/java/nl/rmokveld/castnotifications/WifiStateReceiver.java
@@ -3,25 +3,27 @@ package nl.rmokveld.castnotifications;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 
 public class WifiStateReceiver extends BroadcastReceiver {
 
     private static final String TAG = "WifiStateReceiver";
-    private Boolean mWasConnected;
+    private static Boolean sWasConnected;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
             Log.d(TAG, "onWifiStateChanged()");
-            boolean isConnected = DeviceStateHelper.isWifiConnected(context);
-            if (mWasConnected == null || mWasConnected != isConnected) {
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            boolean isConnected = networkInfo.isConnected();
+            if (sWasConnected == null || sWasConnected != isConnected) {
+                sWasConnected = isConnected;
                 if (isConnected) {
                     CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiConnected();
                 } else {
                     CastNotificationManager.getInstance().getDiscoveryStrategy().onWifiDisconnected();
                 }
-                mWasConnected = isConnected;
             }
         }
     }
